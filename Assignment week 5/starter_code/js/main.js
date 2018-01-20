1d3616a88d20e5a6256489e5b97d56d6cbb5ef2a
immaterium <33899618+immaterium@users.noreply.github.com>
2018-01-20 09:56:55 +0800

    Initial Commit

--- Assignment week 5/starter_code/js/main.js
+++ Assignment week 5/starter_code/js/main.js
@@ -11,19 +11,24 @@ $(document).ready(function () {
 		var city = ($('#city-type').val());
 		city = String(city);
 
-		if ($(city) === 'New York'||'New York City'||'NYC') {			
+		if ($(city) === 'New York'||'New York City'||'NYC') {	
+			console.log('clicked NYC')		
 			$('body').css('background-image', 'url(images/nyc.jpg)')
 		}	else if 
 			($(city) === 'San Francisco'||'SF'||'Bay Area') {
+			console.log('clicked SF')
 			$('body').css('background-image', 'url(images/sf.jpg)')
 		}	else if 
 			($(city) === 'Los Angeles'||'LA'||'LAX') {
+			console.log('clicked LA')
 			$('body').css('background-image', 'url(images/la.jpg)')
 		}	else if 
 			($(city) === 'Austin'||'ATX') {
+			console.log('clicked ATX')
 			$('body').css('background-image', 'url(images/austin.jpg)')
 		}	else if 
 			($(city) === 'Sydney'||'SYD') {
+			console.log('clicked SYD')
 			$('body').css('background-image', 'url(images/sydney.jpg)') 
 		}	
 	}
