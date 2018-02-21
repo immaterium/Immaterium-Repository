cdef590c4b648db877603faad008b3b1490d13b6
immaterium <33899618+immaterium@users.noreply.github.com>
2018-02-21 17:20:42 +0800

    Update

--- Final Project/js/main.js
+++ Final Project/js/main.js
@@ -1,10 +1,9 @@
 console.log('javascript file linked to index.html');
 
 $(document).ready(function () {
-	
+
 	console.log('jquery is loaded');
 
-  // Add click event listeners to "Read more >" of each post
   $('#read-more-post-1').click(showMorePost1);
   $('#read-more-post-2').click(showMorePost2);
 
@@ -13,32 +12,16 @@ $(document).ready(function () {
     var postExerpt = $(toggleButton).parent().find('.exerpt');
 
     if ($(postExerpt).data('state') === 'hidden') {
-      // change data-state attribute value to 'show'
       $(postExerpt).data('state', 'show');
-
-      // reveal exerpt with animation (try with show() and slideDown())
       $(postExerpt).show();
-      // $(postExerpt).slideDown();
-
-      // change the text to "Read less"
       $(toggleButton).text('Read less <');
     } else {
-      // change data-state attribute value to 'hidden'
       $(postExerpt).data('state', 'hidden');
-
-      // hide exerpt with animation (try with hide() and slideUp())
       $(postExerpt).hide();
-      // $(postExerpt).slideUp();
-
-      // change text back to "Read more"
       $(toggleButton).text('Read more >');
     }
   }
 
-  /**
-   * Basically the same function as showMorePost1
-   * except without the comments.
-   */
   function showMorePost2() {
     var toggleButton = $('#read-more-post-2');
     var postExerpt = $(toggleButton).parent().find('.exerpt');
